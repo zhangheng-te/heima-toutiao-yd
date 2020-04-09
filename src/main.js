0b65af36703bfd6a61fed3bdd1c66badf55b2d89
张恒 <1479363919@qq.com>
2020-04-09 08:24:51 +0800

    调整结构

--- src/main.js
+++ src/main.js
@@ -3,6 +3,14 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// 引入flexible
+import 'amfe-flexible'
+
+// 导入vant库
+import Vant from 'vant'
+import 'vant/lib/index.css'
+Vue.use(Vant)
+
 Vue.config.productionTip = false
 
 new Vue({
---
+++ src/views/home/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="container">123</div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped lang='less'>
+.container {
+  width: 100px;
+  height: 100px;
+}
+</style>
